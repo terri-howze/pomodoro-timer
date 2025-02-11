b7e502c8412597749b2fb0769604dd4abca79910
Terri Laptop <howzeterri26@gmail.com>
2025-02-11 15:22:01 -0500

    Initialized Pomodoro project with nextjs, tailwindcss, and tauri

---
+++ TimerPage/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <div>
+      </div>
+      <div >
+
+      </div>
+      <div>
+
+      </div>
+    </>
+  )
+}
