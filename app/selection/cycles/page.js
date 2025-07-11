7602ac60e5e2692c12a32a56e4f7faaa31854d2b
Terri Laptop <howzeterri26@gmail.com>
2025-07-11 12:50:18 -0400

    Removed all a tags to test stopping reload error on prod

--- app/selection/cycles/page.js
+++ app/selection/cycles/page.js
@@ -59,20 +59,20 @@ export default function cycles() {
 
     return (
         <>
-            <a onClick={homePage}>
-                <div className="ml-24 mt-5 text-4xl drop-shadow-2xl mb-0 pl-2 flex">
+            
+                <div onClick={homePage} className="ml-24 mt-5 text-4xl drop-shadow-2xl mb-0 pl-2 flex">
                     <img src='/PJ Logo.png' className='h-20 -mt-2' ></img>
                     <h1 className='text-textColor mt-4 ml-4'>Productivity Jam</h1>
-                </div></a>
+                </div>
             <div className={`flex justify-center`}>
                 <div className='bg-overlay w-screen h-mainDivVh ml-24 mr-24 mt-2 text-lavender flex justify-center rounded-lg'>
                     <div className={`flex justify-end bottom-0 w-innerboxW h-innerboxH transition-opacity duration-1000 `}>
                         <div className="w-innerboxW h-innerboxH text-center">
                             <h2 className='pr-4 mt-20 text-4xl'>Cycles</h2>
                             <div className="flex justify-evenly pt-24">
-                                <a onClick={() => setCycles(3)}><div tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${cycles === 3 ? 'bg-buttons text-textColor' : ''}`}>3</div></a>
-                                <a onClick={() => setCycles(4)}><div tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${cycles === 4 ? 'bg-buttons text-textColor' : ''}`}>4</div></a>
-                                <a onClick={() => setCycles(5)}><div tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons  hover:bg-lavender hover:text-textColor ${cycles === 5 ? 'text-textColor bg-buttons ' : ''}`}>5</div></a>
+                                <div onClick={() => setCycles(3) }tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${cycles === 3 ? 'bg-buttons text-textColor' : ''}`}>3</div>
+                                <div onClick={() => setCycles(4)} tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${cycles === 4 ? 'bg-buttons text-textColor' : ''}`}>4</div>
+                                <div onClick={() => setCycles(5)} tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons  hover:bg-lavender hover:text-textColor ${cycles === 5 ? 'text-textColor bg-buttons ' : ''}`}>5</div>
                             </div>
                             <div className=" flex justify-evenly pt-20 ">
                                 <button onClick={previousPage} className="px-4 py-2 text-2xl border-2 border-lavender hover:bg-lavender hover:text-textColor shadow-md  active:bg-buttons ">
--- app/selection/longbreak/page.js
+++ app/selection/longbreak/page.js
@@ -56,20 +56,20 @@ export default function longBreak() {
     }, []);
     return (
         <>
-            <a onClick={homePage}>
-                <div className="ml-24 mt-5 text-4xl drop-shadow-2xl mb-0 pl-2 flex">
+            
+                <div onClick={homePage} className="ml-24 mt-5 text-4xl drop-shadow-2xl mb-0 pl-2 flex">
                     <img src='/PJ Logo.png' className='h-20 -mt-2' ></img>
                     <h1 className='text-textColor mt-4 ml-4'>Productivity Jam</h1>
-                </div></a>
+                </div>
             <div className={`flex justify-center`}>
                 <div className='bg-overlay w-screen h-mainDivVh ml-24 mr-24 mt-2 text-lavender flex justify-center rounded-lg'>
                     <div className='flex justify-end bottom-0 w-innerboxW h-innerboxH'>
                         <div className="w-innerboxW h-innerboxH text-center">
                             <h2 className='pr-4 mt-20 text-4xl'>Long Break</h2>
                             <div className="flex justify-evenly pt-24">
-                                <a onClick={() => setLongBreak(15)}><div tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${longBreak === 15 ? 'bg-buttons text-textColor' : ''}`}>15</div></a>
-                                <a onClick={() => setLongBreak(20)}><div tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${longBreak === 20 ? 'bg-buttons text-textColor' : ''}`}>20</div></a>
-                                <a onClick={() => setLongBreak(25)}><div tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${longBreak === 25 ? 'bg-buttons text-textColor' : ''}`}>25</div></a>
+                                <div onClick={() => setLongBreak(15)} tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${longBreak === 15 ? 'bg-buttons text-textColor' : ''}`}>15</div>
+                                <div onClick={() => setLongBreak(20)} tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${longBreak === 20 ? 'bg-buttons text-textColor' : ''}`}>20</div>
+                                <div onClick={() => setLongBreak(25)} tabIndex="0" className={`size-20 flex items-center justify-center rounded-lg text-3xl border-2 border-lavender active:bg-buttons hover:bg-lavender hover:text-textColor ${longBreak === 25 ? 'bg-buttons text-textColor' : ''}`}>25</div>
                             </div>
                             <div className=" flex justify-evenly pt-20">
                                 <button onClick={previousPage} className="px-4 py-2 text-2xl border-2 border-lavender hover:bg-lavender hover:text-textColor shadow-md  active:bg-buttons">
